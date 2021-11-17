fba9e42532792f915012fc045998f686af3b95ec
mraimbot <mraimbot@hotmail.de>
2021-11-17 12:49:54 +0100

    fixed compile errors

--- Assets/Scripts/MainManager.cs
+++ Assets/Scripts/MainManager.cs
@@ -90,6 +90,7 @@ public class MainManager : MonoBehaviour
 
     private void SetBestScore()
     {
+        Debug.Log(GameManager.Instance.highscores.Length);
         var highscore = GameManager.Instance.highscores[0];
         textBestScore.text = "Best Highscore: " + highscore.name + " with " + highscore.score + " points.";
     }
